3808cf06316d4783ae349f4af8022286065f5627
programmerarantes <programmerarantes@gmail.com>
2024-09-20 10:00:54 -0300

    fix: updating cors to localhost

--- config/middlewares.js
+++ config/middlewares.js
@@ -18,7 +18,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://depapocomasdoulas.netlify.app'], // Permite todas as origens, ajuste conforme necessário
+      origin: ['https://depapocomasdoulas.netlify.app', 'http://localhost:5173/'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
     },
